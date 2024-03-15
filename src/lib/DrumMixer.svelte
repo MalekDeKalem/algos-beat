b41c30e5ce925c8f0e95bb67095ab60ed4d73fdd
MalekDeKalem <dimitri200169@gmail.com>
2024-03-15 13:12:59 +0100

    prompts for DrumMixer

---
+++ src/lib/DrumMixer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let gainKick: number;
+    export let gainSnare: number;
+    export let gainClap: number;
+    export let gainHat: number;
+    export let gainPerc: number;
+    export let gainPerc2: number;
+</script>
+
+
+
+
+
+<div>
+
+</div>
+
+
+
+<style>
+    
+</style>
--- src/stores.ts
+++ src/stores.ts
@@ -5,4 +5,4 @@ export const bpmStore = writable(120);
 export const beatStore = writable(0);
 export const playStore = writable(false);
 export const drumEffectStore = writable<number | null>(null);
-export const synthEffectStore = writable(0);
+export const synthEffectStore = writable<number | null>(null);
