d2d1e25e980afe9c7e443208c67c316eaa52f321
MalekDeKalem <dimitri200169@gmail.com>
2024-04-29 12:06:44 +0200

    added props to the component

---
+++ src/lib/Slider.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { onMount, beforeUpdate, afterUpdate } from 'svelte';
+
+  let border_size = 2;
+  let isDragging = false;
+  export let size = 100;
+  export let min = 0;
+  export let max = 100;
+  export let value = 100.0;
+  export let steps = 10;
+
+
+  export let updateValue: (number) => void = (newValue) => {};
+</script>
+
+
+
+
+
+<canvas id="slider"> </canvas>
+
+
+
+
+<styles>
+
+
+</styles>
